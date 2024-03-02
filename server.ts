7cd04ffbe4291e1b433429646a11ce13bdd98efe
wanda yusuf alvian <wanda.alfian@mceasy.co.id>
2024-03-02 09:34:39 +0700

    g

--- server.ts
+++ server.ts
@@ -8,6 +8,7 @@ import dotenv from 'dotenv';
 
 async function main() {
   const app = express();
+  const redisClient = await getRedisClient();
   app.use(express.json());
   dotenv.config();
 
@@ -22,18 +23,35 @@ async function main() {
   // register user 
   app.post("/register", async (req, res) => {
 
+    // save user data to db with is_active = false
     await repo.addUser(db, {
       email: req.body.email,
       password: req.body.password,
       is_active: false,
     });
 
+    // generate otp, save to redis with expiry time, and then send otp to user 
     const OTP = generate();
     console.log(OTP);
+    await redisClient.set(req.body.email, OTP);
+    await redisClient.expire(req.body.email, 60);
     await email.sendToEmail(req.body.email, OTP);
     res.send("register success, next enter otp sent to your email to activate your account");
     res.status(200);
   });
+
+
+
+  app.post('/verify', async (req, res) => {
+
+  });
+
+  app.post('/resend', async (req, res) => {
+    {
+
+    }
+  });
+
 
   // listen to port
   const port = process.env.PORT || 3000;
